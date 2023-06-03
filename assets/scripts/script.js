6d9bd94cfa20bca3c6bdb41d5d3c0b329c7431c3
sebastxx <andres.bareiro96@gmail.com>
2023-06-03 20:34:28 -0300

    gp-workflow

---
+++ assets/scripts/script.js
@@ -0,0 +1,60 @@
+// Configura las credenciales y la ID de la hoja de cálculo
+const SPREADSHEET_ID = '17CrG87_7vm2FDNAeo067H-fexDHf9_Y9xMCTmdRBnJI';
+const CLIENT_ID = '721501934315-ei5o1la165isfkrajj1fpjpt4l8cneg6.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyC6UvksN7iFu6HetUO0gAt9cgoZPZfNDno';
+
+function cargarDatos() {
+
+    //           https://sheets.googleapis.com/v4/spreadsheets/17CrG87_7vm2FDNAeo067H-fexDHf9_Y9xMCTmdRBnJI/values/Hoja!A:L,majorDimension=COLUMNS?key=AIzaSyC6UvksN7iFu6HetUO0gAt9cgoZPZfNDno`;
+
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/Hoja!A:L?key=${API_KEY}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Obtener los datos de la respuesta
+            const rows = data.values;
+
+            // Obtener el contenedor de tarjetas
+            const cardContainer = document.querySelector('.card-container');
+            cardContainer.innerHTML = '';
+
+            rows.forEach(row => {
+                const numOrden = row[1];
+                const producto = row[2];
+                const descripcion = row[3];
+                const responsable = row[4];
+                const telefono = row[5];
+
+                // Crear la tarjeta
+                const card = document.createElement('div');
+                card.classList.add('card');
+
+                // Agregar contenido a la tarjeta
+                const numOrdenElement = document.createElement('h3');
+                numOrdenElement.textContent = `Número de orden: ${numOrden}`;
+                card.appendChild(numOrdenElement);
+
+                const productoElement = document.createElement('p');
+                productoElement.textContent = `producto: ${producto}`;
+                card.appendChild(productoElement);
+
+                const descripcionElement = document.createElement('p');
+                descripcionElement.textContent = `Descripción: ${descripcion}`;
+                card.appendChild(descripcionElement);
+
+                const responsableElement = document.createElement('p');
+                responsableElement.textContent = `Realizado por: ${responsable}`;
+                card.appendChild(responsableElement);
+
+                // Agregar la tarjeta al contenedor
+                cardContainer.appendChild(card);
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar los datos:', error);
+        });
+}
+
+// Cargar los datos al cargar la página
+window.addEventListener('load', cargarDatos);
